a721dee5b6b4e84a58aec499c2a79bda19339f75
lanwp <lanwp530@163.com>
2019-03-28 02:08:31 +0800

    观察者模式

---
+++ design-patterns/observer/src/main/java/com/lwp/dp/observer/events/Keybord.java
@@ -0,0 +1,18 @@
+package com.lwp.dp.observer.events;
+
+import com.lwp.dp.observer.events.core.EventLisenter;
+
+/**
+ * Created by lanwp on 2019/3/28
+ */
+public class Keybord extends EventLisenter {
+
+    public void down(){
+
+    }
+
+    public void up(){
+
+    }
+
+}
---
+++ design-patterns/observer/src/main/java/com/lwp/dp/observer/events/MouseEventTest.java
@@ -0,0 +1,23 @@
+package com.lwp.dp.observer.events;
+
+/**
+ * Created by lanwp on 2019/3/28.
+ */
+public class MouseEventTest {
+    public static void main(String[] args) {
+
+        MouseEventCallback callback = new MouseEventCallback();
+
+        Mouse mouse = new Mouse();
+
+        //@谁？  @回调方法
+        mouse.addLisenter(MouseEventType.ON_CLICK,callback);
+        mouse.addLisenter(MouseEventType.ON_FOCUS,callback);
+
+        mouse.click();
+
+        mouse.focus();
+
+
+    }
+}
